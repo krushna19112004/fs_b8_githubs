72c6ef5fa59332aa8504dfe1a10a0bd09cc94817
kiranugale2o <kiranugale129@gmail.com>
2025-07-30 18:05:49 +0530

    second commit

---
+++ first.java
@@ -0,0 +1,6 @@
+public class first {
+ public static void main(string[] args){
+  system.out.println(x:"hello from the first file!");
+    system.out.println(x:"callingdfgd second file main method:!") 
+ }
+}
