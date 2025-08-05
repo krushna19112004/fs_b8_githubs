0b7ce25c6fe25083115ed1b37cc7143dabc67974
krushna19112004 <hamandkrushna@gmail.com>
2025-08-05 13:45:22 +0530

    done

---
+++ age.java
@@ -0,0 +1,12 @@
+
+public class age {
+    public static void main(String[] args) {
+        int age = 19;
+        if( age <= 18){
+            System.out.println("child");
+        } else{
+        System.out.println("adult");
+        }
+    }
+}
+
